d65bc3d2058011656ddf4a3aad03b0432da08c03
mohamediyas <mohamedimthiyas24@gmail.com>
2021-06-19 22:27:27 -0700

    react query boiler plate

---
+++ .history/src/PostDetail_20210619220233.jsx
@@ -0,0 +1,82 @@
+import { useQuery, useMutation } from "react-query";
+async function fetchComments(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+  );
+  return response.json();
+}
+
+async function deletePost(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "DELETE" }
+  );
+  return response.json();
+}
+
+async function updatePost(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+  );
+  return response.json();
+}
+
+export function PostDetail({ post }) {
+  // replace with useQuery
+  const { data, isLoading, isError, error } = useQuery(
+    ["comments", post.id],
+    () => fetchComments(post.id),
+    {
+      staleTime: 2000,
+    }
+  );
+
+  const deletedPost = useMutation((postId) => deletePost(postId));
+
+  const updatedPost = useMutation((postId) => updatePost(postId));
+
+  if (isLoading) return <h1>Loading....</h1>;
+
+  if (isError) {
+    return (
+      <>
+        <h1>Something went wrong</h1>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <button onClick={() => deletedPost.mutate(post.id)}>Delete</button>
+      {deletedPost.isLoading && (
+        <p style={{ color: "blue" }}>Deleting........</p>
+      )}
+      {deletedPost.isError && (
+        <p style={{ color: "red" }}>Unable to delete........</p>
+      )}
+      {deletedPost.isSuccess && (
+        <p style={{ color: "green" }}>Post deleted.......</p>
+      )}
+      <button onClick={() => updatedPost.mutate(post.id)}>Update title</button>
+      {updatedPost.isLoading && (
+        <p style={{ color: "blue" }}>Post updating.....</p>
+      )}
+      {updatedPost.isError && (
+        <p style={{ color: "red" }}>Something went wrong.....</p>
+      )}
+      {updatedPost.isSuccess && (
+        <p style={{ color: "green" }}>Post updated.....</p>
+      )}
+      <p>{post.body}</p>
+      <h4>Comments</h4>
+      {data.map((comment) => (
+        <li key={comment.id}>
+          {comment.email}: {comment.body}
+        </li>
+      ))}
+    </>
+  );
+}
---
+++ .history/src/Posts_20210619211826.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+import { PostDetail } from "./PostDetail";
+const maxPostPage = 10;
+
+async function fetchPosts(page) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
+  );
+  return response.json();
+}
+
+export function Posts() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  // replace with useQuery
+  const { isLoading, isError, error, data } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+    }
+  );
+
+  //isloading
+  if (isLoading) return <h1>Is Loading.......</h1>;
+
+  // iserroe
+  if (isError)
+    return (
+      <>
+        {" "}
+        <h1>Something went wrong</h1> <p>{error.toString()}</p>{" "}
+      </>
+    );
+
+  return (
+    <>
+      <ul>
+        {data.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((prewpage) => prewpage - 1);
+          }}
+        >
+          Previous page
+        </button>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((prewpage) => prewpage + 1);
+          }}
+        >
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+}
